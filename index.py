f46c7cc7dce2c88026ea392ec83cf8428d321eba
ludwik2dev <140020505+ludwik2dev@users.noreply.github.com>
2024-12-19 05:47:28 +0100

    First callback

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc, callback, Output, Input
 
 import input
 
@@ -11,10 +11,23 @@ app = Dash(__name__)
 app.layout = html.Div([
     html.Div('Hello World'),
     dcc.Dropdown(
+        id='id-dropdown',
         options=units,
         value=units[1],  # 'Coal 2',
-    )
+    ),
+    html.H4(id='id-output'),
 ])
+
+
+@callback(
+    Output('id-output', 'children'),
+    Input('id-dropdown', 'value'),
+    prevent_initial_call=True
+)
+def select_dropdown(value):
+
+    return f'User chose: {value}'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
